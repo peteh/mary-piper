7e5f6df99f59648aea921b88c9e57c172a466830
Pete <github@kthxbye.us>
2024-04-18 17:36:09 +0200

    ADD: voice caching

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM python:3.10-bookworm
 #VOLUME /storage
-RUN mkdir -p /root/.cache && ln -s /storage /root/.cache/huggingface && mkdir -p /root/.local && ln -s /storage /root/.local/share
+RUN mkdir -p /cache_voices/ && mkdir -p /custom_voices/
 #RUN apt-get update && apt-get install -y opus-tools vorbis-tools ffmpeg espeak
 WORKDIR /app
 COPY app/ .
 RUN pip3 install -U -r requirements.txt
-CMD ["python3", "-u", "server.py"]
+CMD ["python3", "-u", "server.py", "--cache-dir", "/cache_voices/", "--custom-voice-dir", "/custom_voices/" ]
 
 ENV SERVER_PORT 59125
 EXPOSE 59125/tcp
--- app/server.py
+++ app/server.py
@@ -15,20 +15,105 @@ from piper.download import ensure_voice_exists, find_voice, get_voices
 
 _LOGGER = logging.getLogger()
 # TODO: differnt path than ./ does currently not work
-DATA_DIR = "./"
+DEFAULT_VOICE_CACHE_DIR = "./data/"
+DEFAULT_VOICE_CUSTOM_DIR = "./custom/"
 HOST = "0.0.0.0"
 PORT = 59125
 
 class ServerApp():
-    def __init__(self, default_voice: str) -> None:
-        self._voices = get_voices(DATA_DIR, update_voices=True)
+    def __init__(self, voice_cache_dir: str, voice_custom_dir: str, default_voice: str) -> None:
+        self._voice_cache_dir = voice_cache_dir
+        self._voice_custom_dir = voice_custom_dir
+        self._voices = get_voices(self._voice_cache_dir, update_voices=True)
     
-    def voices(self) -> List[str]:
+    def _get_voices(self) -> List[str]:
         return self._voices
+    
+    def mary_voices(self):
+        lines = []
+        voices = self._voices
+        for voice_name in voices:
+            voice = voices[voice_name]
+            num_speakers = voice["num_speakers"]
+            language_code = voice["language"]["code"]
+            if num_speakers > 1:
+                speaker_map = voice["speaker_id_map"]
+                for speaker in speaker_map:
+                    line = f"{voice_name}:{speaker} {language_code} NA VITS"
+                    lines.append(line)
+                print(voice)
+            else:
+                line = f"{voice_name} {language_code} NA VITS"
+                lines.append(line)
+        lines.sort()
+        return "\n".join(lines)
+    
+    def mary_process(self, request):
+        voices_info = self._get_voices()
+        voice_name = "NO VOICE SELECTED"
+        speaker_id = None
+        if request.method == "POST":
+            data = parse_qs(request.data.decode())
+            text = data.get("INPUT_TEXT", [""])[0]
+
+            if "VOICE" in data:
+                voice_name = str(data.get("VOICE", [voice_name])[0]).strip()
+        else:
+            text = request.args.get("INPUT_TEXT", "")
+            voice_name = str(request.args.get("VOICE", voice_name)).strip()
+
+        if ":" in voice_name:
+            speaker_name = voice_name[voice_name.find(":")+1:]
+            
+            voice_name = voice_name[:voice_name.find(":")]
+            print(voices_info[voice_name])
+            speaker_id = voices_info[voice_name]["speaker_id_map"][speaker_name]
+        print(voice_name)
+        print(speaker_id)
+        print(text)
+        try:
+            onnx_path, config_path = find_voice(voice_name, [self._voice_cache_dir, self._voice_custom_dir])
+        except ValueError:
+            ensure_voice_exists(voice_name, [self._voice_cache_dir], self._voice_cache_dir, voices_info)
+            onnx_path, config_path = find_voice(voice_name, [self._voice_cache_dir, self._voice_custom_dir])
+
+        voice = PiperVoice.load(onnx_path, config_path=None, use_cuda=False)
+        
+        synthesize_args = {
+            "speaker_id": speaker_id,
+            "length_scale": None,
+            "noise_scale": None,
+            "noise_w": None,
+            "sentence_silence": 0.0,
+        }
+        with io.BytesIO() as wav_io:
+            with wave.open(wav_io, "wb") as wav_file:
+                voice.synthesize(text, wav_file, **synthesize_args)
+
+            return wav_io.getvalue()
 
 
 def main() -> None:
-    server_app = ServerApp("en_US-lessac-medium")
+    parser = argparse.ArgumentParser(description="Mary Piper TTS Web API")
+    
+    # Argument for cache directory
+    parser.add_argument('--cache-dir', dest='cache_dir', type=str, default=DEFAULT_VOICE_CACHE_DIR,
+                        help='Directory for caching voices that are downloaded')
+
+    # Argument for voice directory
+    parser.add_argument('--custom-voice-dir', dest='custom_voice_dir', type=str, default=DEFAULT_VOICE_CUSTOM_DIR,
+                        help='Directory for custom voices')
+
+    args = parser.parse_args()
+
+    # Using the provided or default directories
+    voice_cache_dir = args.cache_dir
+    voice_custom_dir = args.custom_voice_dir
+
+    print("Cache directory:", voice_cache_dir)
+    print("Voice directory:", voice_custom_dir)
+    
+    server_app = ServerApp(voice_cache_dir, voice_custom_dir, "en_US-lessac-medium")
 
     # Create web server
     app = Flask(__name__)
@@ -54,65 +139,12 @@ def main() -> None:
 
     @app.route("/voices", methods=["GET"])
     def voices():
-        lines = []
-        voices = server_app.voices()
-        for voice_name in voices:
-            voice = voices[voice_name]
-            num_speakers = voice["num_speakers"]
-            language_code = voice["language"]["code"]
-            if num_speakers > 1:
-                speaker_map = voice["speaker_id_map"]
-                for speaker in speaker_map:
-                    line = f"{voice_name}:{speaker} {language_code} NA VITS"
-                    lines.append(line)
-                print(voice)
-            else:
-                line = f"{voice_name} {language_code} NA VITS"
-                lines.append(line)
-        lines.sort()
-        return "\n".join(lines)
+        return server_app.mary_voices()
 
     @app.route("/process", methods=["GET", "POST"])
     def api_marytts_process():
         """MaryTTS-compatible /process endpoint"""
-        voices_info = server_app.voices()
-        voice_name = "NO VOICE SELECTED"
-        speaker_id = None
-        if request.method == "POST":
-            data = parse_qs(request.data.decode())
-            text = data.get("INPUT_TEXT", [""])[0]
-
-            if "VOICE" in data:
-                voice_name = str(data.get("VOICE", [voice_name])[0]).strip()
-        else:
-            text = request.args.get("INPUT_TEXT", "")
-            voice_name = str(request.args.get("VOICE", voice_name)).strip()
-
-        if ":" in voice_name:
-            speaker_name = voice_name[voice_name.find(":")+1:]
-            
-            voice_name = voice_name[:voice_name.find(":")]
-            print(voices_info[voice_name])
-            speaker_id = voices_info[voice_name]["speaker_id_map"][speaker_name]
-        print(voice_name)
-        print(speaker_id)
-        print(text)
-        
-        ensure_voice_exists(voice_name, [DATA_DIR], DATA_DIR, voices_info)
-        voice = PiperVoice.load(f"{voice_name}.onnx", config_path=None, use_cuda=False)
-        
-        synthesize_args = {
-            "speaker_id": speaker_id,
-            "length_scale": None,
-            "noise_scale": None,
-            "noise_w": None,
-            "sentence_silence": 0.0,
-        }
-        with io.BytesIO() as wav_io:
-            with wave.open(wav_io, "wb") as wav_file:
-                voice.synthesize(text, wav_file, **synthesize_args)
-
-            return wav_io.getvalue()
+        return server_app.mary_process(request)
 
     app.run(host=HOST, port=PORT)
 
